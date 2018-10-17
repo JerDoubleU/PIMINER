a31c12514891ec3ed3f0c05207597a37df5665db
CooperStansbury <cstansbu@umich.edu>
2018-10-17 17:29:20 -0400

    expanded regex abilities and defined program flow control better by moving repeated calls into new functions

--- PIMINER.py
+++ PIMINER.py
@@ -4,19 +4,70 @@ import argparse # inorder to port to command line
 import os # system controls
 import pandas as pd # dataframe data structure for outputs
 import textract # used to parse PDF files
-import spacy # industrial strength NLP engine
 import re # string operations, mostly data cleaning
 import subprocess # used to invoke shell scripts
+import spacy # industrial strength NLP engine
+from spacy.symbols import nsubj, VERB
 
-# main function call
-def PIMINER(input_file):
 
-    model = 'en_core_web_lg'
+"""
+Iterate through a series of regex expressions for matches
+Expressions defined in the piminer() function
+"""
+def getRegexMatches(patterns, doc):
+
+    regex_matches = []
+
+    for search_pattern in patterns:
+        search_cout = 0
+        print('Searching for: ' + str(search_pattern[0]))
+
+        for regex_match in re.finditer(re.compile(search_pattern[1]), doc.text):
+            if regex_match:
+                search_cout += 1
+                match_as_string = "{}".format(regex_match.group(0))
+                tup = (search_pattern[0], match_as_string)
+                regex_matches.append(tup)
+
+        print(str(search_pattern[0] + ' search COMPLETE.'))
+        print('Found ' + str(search_cout) + '\n')
+
+    return regex_matches
+
+
+"""
+Iterate through named_entity for matches of certain types
+entity_types defined in piminer() function
+"""
+def getNamedEntities(entity_types, doc):
+    print('Searching for named_entity...')
+
+    valid_ent = re.compile("^[a-zA-Z0-9_]*$")
+    ENTITY_LIST = []
+
+    for entity in doc.ents:
+        # for now, limit output to the entity types in the list above,, clean output
+        if entity.label_ in entity_types:
+            NER_tup = (str(entity.label_).upper(), re.sub('[^A-Za-z0-9]+', ' ', str(entity.text)))
+            ENTITY_LIST.append(NER_tup)
+
+    print('named_entity search COMPLETE.')
+    print('Found' + str(len(ENTITY_LIST)) + '\n')
+    return ENTITY_LIST
+
+"""
+Main control function
+Call aux functions and print statuses to stdin
+"""
+def piminer(input_file):
+
+    # model = 'en_core_web_lg'
+    model = 'en_core_web_md'
     print('Loading spacy library: ' + str(model))
 
     # load NLP library object
     nlp = spacy.load(model)
-    print('Library load COMPLETE.')
+    print('Library load COMPLETE.\n')
 
     # file operations
     source_file = os.path.basename(input_file)
@@ -26,52 +77,30 @@ def PIMINER(input_file):
     need to make simple file handling more robust.
     it may be the case that this is better suited for another script,
     or should be called from main. TDB.
-
-    possible to invoke shell cleaning script from this script like below:
-
-    # # this code can trigger shell script cleaning operations
-    # cleanFiles = 'text_cleaner.sh'
-    # process = subprocess.Popen(cleanFiles.split(), stdout=subprocess.PIPE)
-    # output, error = process.communicate()
     """
 
     print('Reading file: ' + str(source_file))
     # create nlp object from input_file
     text = str(textract.process(input_file))
-    print('Read COMPLETE.')
+    print('Read COMPLETE.\n')
 
     print('Converting file to spacy object: ' + str(source_file))
     doc = nlp(text)
-    print('Convert COMPLETE.')
+    print('Convert COMPLETE.\n')
 
-    print('Checking ' + str(base) + ' for phone numbers...')
-    ## this is a check for possible phone numbers, saved to list
-    ## Need to look for 9 digits phone numbers
-    PHONE_NUMBER = re.compile('\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4}')
-    PHONE_NUMBER_LIST = []
+    patterns = [
+        ('phone_number', '\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4}'),
+        ('email_address', '^[a-zA-Z0-9._%-+]+@[a-zA-Z0-9._%-]+.[a-zA-Z]{2,6}$'),
+        ('social_security_number', '^(?!000)([0-6]\d{2}|7([0-6]\d|7[012])) ([ -])? (?!00)\d\d([ -|])? (?!0000)\d{4}$'),
+        ('EIN_number', '/[0-9]{2}-[0-9]{7}/'),
+        ('passport_number', '/[0-9]{2}-[0-9]{7}/'),
+        ('Iv4', '/[0-9]{2}-[0-9]{7}/'),
+        ('Iv6', '/^(?:[A-F0-9]{1,4}:){7}[A-F0-9]{1,4}$/i'),
+        ('credit_card_number', '/^(?:[A-F0-9]{1,4}:){7}[A-F0-9]{1,4}$/i')
+    ]
 
-    for phone_match in re.finditer(PHONE_NUMBER, doc.text):
-        if phone_match:
-            PHONE_NUMBER_LIST.append("{}".format(phone_match.group(0)))
-            # print("POSSIBLE PHONE NUMBER: {}".format(phone_match.group(0)))
-    print('Phone number analysis COMPLETE.')
-
-    print('Checking ' + str(source_file) + ' for email addresses...')
-    ## this is a check for possible email addresses, saved to a list
-    # EMAIL_ADDRESS = re.compile('[^@]+@[^@]+\.[^@]+')
-    # EMAIL_ADDRESS_LIST = []
-    #
-    # for email_match in re.finditer(EMAIL_ADDRESS, doc.text):
-    #     if email_match:
-    #         PHONE_NUMBER_LIST.append("{}".format(email_match.group(0)))
-    #         print("POSSIBLE EMAIL ADDRESS: {}".format(email_match.group(0)))
-    print('Email addresses analysis COMPLETE.')
-
-    """
-    ## this is a list of spacy entities types
-    ## that may be useful for tagging when checking for PII
-    ## THIS LIST IS NOT EXHAUSTIVE
-    """
+    # define lists based off of first-pass regex and NER
+    getRegexMatches(patterns, doc)
 
     entity_types = ['PERSON', # People, including fictional.
                     'NORP', # Nationalities or religious or political groups.
@@ -82,31 +111,17 @@ def PIMINER(input_file):
                     'EVENT', # Named hurricanes, battles, wars, sports events, etc
                     'WORK_OF_ART', # Titles of books, songs, etc.
                     'LAW', # Named documents made into laws.
-                    'DATE' # Absolute or relative dates or periods.
-                    ]
-    print('Checking ' + str(source_file) + ' for names, organizations, and dates...')
-    ENTITY_LIST = []
-    # print out detected named entities
-    for entity in doc.ents:
+                    'DATE'] # Absolute or relative dates or periods.
 
-        # for now, limit output to the entity types in the list above
-        if entity.label_ in entity_types:
-            ENTITY_LIST.append(str(entity.label_).upper() + ": " + str(entity.text))
-            # print('POSSIBLE ' + str(entity.label_).upper() + ": " + str(entity.text))
-
-    # # iteration through file and print noun chunks
-    # for chunk in doc.noun_chunks:
-    #     print(chunk)
-
-    ### need to check dependencies
+    getNamedEntities(entity_types, doc)
 
 
 if __name__ == "__main__":
 
     # parse command-line args
     parser = argparse.ArgumentParser(description='file')
-    parser.add_argument("input_file", help="choose the text file to process.")
+    parser.add_argument("input", help="Choose the text file to process.")
     args = parser.parse_args()
 
     # run puppy, run
-    PIMINER(args.input_file)
+    piminer(args.input)
