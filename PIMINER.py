f146ad76b25d3781a5884bcaf5539ce6c2ce1f3f
CooperStansbury <cstansbu@umich.edu>
2018-10-26 13:40:55 -0400

    stub a relation finder (not working)
    add timing statements to stdout

--- PIMINER.py
+++ PIMINER.py
@@ -7,6 +7,7 @@ import math # # used to break files into chunks
 import textract # # used to parse PDF files
 import re # # string operations, mostly data cleaning
 import spacy # # industrial strength NLP engine
+import time # used to print timing metrics
 from spacy.symbols import nsubj, VERB
 
 # # function to handle input larger than 1000000 character
@@ -64,32 +65,51 @@ def getEntities(document):
     # # list structure to transform into dataframe
     new_rows = []
 
+    # # merge entities and noun chunks into one token
+    # spans = list(document.ents) + list(document.noun_chunks)
+    # for span in spans:
+    #     span.merge()
+
     print('Conducting named entity search...')
+    nerTime = time.time()
 
     for entity in document.ents:
-
-        # print(dir(entity))
-
-        # print('for:', entity.text)
-
         # store the left and right tokens for all NEs
         lefts = []
+        rights = []
         subtree = []
+        relations = []
 
         [lefts.append(x) for x in entity.lefts]
-        [rights.append(x) for x in entity.lefts]
+        [rights.append(x) for x in entity.rights]
         [subtree.append(x) for x in entity.subtree]
+
+        # if entity.dep_ in ('attr', 'dobj'):
+        #     subject = [w for w in entity.head.lefts if w.dep_ == 'nsubj']
+        #     if subject:
+        #         subject = subject[0]
+        #         relations.append((subject, entity))
+        # elif entity.dep_ == 'pobj' and entity.head.dep_ == 'prep':
+        #     relations.append((entity.head.head, entity))
+
+        # print('for', entity.text)
+        # [print(x) for x in realtions]
+        # print()
 
         row = {'entity_type':str(entity.label_).upper(),
             'text_value':str(entity.text),
             'start_position':entity.start_char,
             'end_position':entity.end_char,
             'lefts':lefts,
+            'rights':rights,
             'subtree':subtree}
 
         new_rows.append(row)
 
-    print('Named entity search COMPLETE.\n')
+    nerTimeEnd = time.time()
+    print('Named entity search COMPLETE: ' + str(nerTimeEnd - nerTime) + ' seconds\n')
+
+    regexTime = time.time()
 
     # # easily extendable regex_patterns
     regex_patterns = {
@@ -112,66 +132,61 @@ def getEntities(document):
         for regex_match in re.finditer(re.compile(regex_patterns[search_pattern]), document.text):
             if regex_match:
 
-                # get position of match
+                # # get position of match
+                # # not sure of this is correct,
+                # # pulling from the document text, not the doc object
                 span = regex_match.span()
-
-                # token = document[span[0]:span[1]]
-                # print(token)
-
 
                 match_as_string = "{}".format(regex_match.group(0))
 
-                # build row
+                # # build row
                 row = {'entity_type':search_pattern,
                     'text_value':match_as_string,
                     'start_position':span[0],
                     'end_position':span[1]}
 
-                # save to list for conversion to dataframe
+                # # save to list for conversion to dataframe
                 new_rows.append(row)
 
-    print('Regex search COMPLETE.\n')
+    regexTimeEnd = time.time()
+    print('Regex search COMPLETE: ' + str(regexTimeEnd - regexTime) + ' seconds\n')
 
     print('Found ' + str(len(new_rows)) + ' total entities...')
     print('Individual entity search COMPLETE.\n')
+
     # # return dataframe with results for clustering
     return pd.DataFrame(new_rows)
-
 
 # # flow control function
 # # takes a a commandline argument (file)
 def piminer(input_file):
 
+    modelLoadTime = time.time()
+
     # # model = 'en_core_web_lg'
     model = 'en_core_web_md'
+
     print('Loading language library: ' + str(model) + '...')
-
-    # # load NLP library object
     nlp = spacy.load(model)
-    print('Library load COMPLETE.\n')
+    modelLoadTimeEnd = time.time()
 
-    # # file operations to manage printing
-    # # probably need to get full path as well...
+    print('Library load COMPLETE: ' + str(modelLoadTimeEnd - modelLoadTime) + ' seconds\n')
+
+    # file operstions for naming output
     source_file = os.path.basename(input_file)
     base = os.path.splitext(source_file)[0]
 
     # # need to make simple file handling more robust
-    # # it may be the case that this is better suited for another script,
-    # # or should be called from main. TDB.
-
     print('Reading file: ' + str(source_file) + '...')
+    fileReadTime = time.time()
 
     # # create nlp object from input_file
     text = str(textract.process(input_file,
             method='tesseract',
             language='en'))
 
-    print('Read COMPLETE.\n')
-
-
-    # # breaks files into manageable chunks
-    # # may be suited for a separate function call
-    # # though likely called only once
+    # # spacy has a limit of 1000000 chars
+    # # break into multiple files to handle
     if len(text) > 999999:
         text_list = []
 
@@ -193,21 +208,28 @@ def piminer(input_file):
     # # index the chunks of the file
     processing_count = 1
 
+    fileReadTimeEnd = time.time()
+    print('Read COMPLETE: ' + str(fileReadTimeEnd - fileReadTime) + ' seconds\n')
+
     # # process all string chunks
     for string_chunk in text_list:
 
+
         print('Converting file to spacy object: ' + str(source_file) + '...')
         print('Part ' + str(processing_count) + ' of ' + str(splits))
+        spacyObjectTime = time.time()
         document = nlp(string_chunk)
-        print('Convert COMPLETE.\n')
+        spacyObjectTimeEnd = time.time()
+        print('Convert COMPLETE: ' + str(spacyObjectTimeEnd - spacyObjectTime) + ' seconds\n')
 
         # # increment the document chunk
         processing_count += 1
 
         # get dataframe with entity type, entity value, and position
         frame = getEntities(document)
-        print(frame)
-        frame.to_csv(str(base) + "_PII_Results.csv")
+        # print(frame)
+        # frame.to_csv(str(base) + "_PII_Results.csv")
+
 
 if __name__ == "__main__":
 
@@ -216,5 +238,9 @@ if __name__ == "__main__":
     parser.add_argument("input", help="Choose the text file to process.")
     args = parser.parse_args()
 
+    totalTime = time.time()
     # # run puppy, run
     piminer(args.input)
+    
+    totalTimeEnd = time.time()
+    print('piminer COMPLETE: ' + str(totalTimeEnd - totalTime) + ' seconds\n')
