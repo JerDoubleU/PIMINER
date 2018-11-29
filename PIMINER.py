39c87ff2cb67e4f9aca5f2b8b92fd418d44e3bf3
CooperStansbury <cstansbu@umich.edu>
2018-11-29 10:36:03 -0500

    added structured comments to piminer

--- PIMINER.py
+++ PIMINER.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 
+"""
+#----------------------------- Authors ----------------------------------------#
+Author: Cooper Stansbury
+Author: Dom Korzecke
+Author: Kevin Peters
+Author: Robert Kaster
+
+#----------------------------- Overview ---------------------------------------#
+Piminer.py is a command line tool written in python3 that can be used to extract
+possible named entities and patterns matching custom regex expressions in
+unstructured text documents.
+
+#----------------------------- Arguments --------------------------------------#
+TWO Positional Arguments:
+1. --input_file: A text file to extract PII elements from.
+2. --regex_input: A text file containing regular expressions and labels.
+"""
+
+#----------------------------- Dependencies -----------------------------------#
 import argparse # # inorder to port to command line
 import os # # system controls
 import pandas as pd # # dataframe data structure for outputs
@@ -11,9 +30,17 @@ import time # used to print timing metrics
 from spacy.symbols import nsubj, VERB
 from tqdm import tqdm # status bar
 
-# # function to handle input larger than 1000000 characters
-# # takes a string (not NLP object) and a list size as an input
+
+################################## Helper Functions ############################
 def textSplit(text, size):
+    """
+    note: Handle doc input larger than 1000000 characters
+    inputs:
+        'text' a string (not NLP object)
+        'size' an integer list size.
+    return: 'text' deivided into 'size' partitions in a list.
+    """
+
     text_splits = []
 
     for i in range(0, len(text), size):
@@ -22,8 +49,14 @@ def textSplit(text, size):
     return text_splits
 
 
-# # function to parse regex input
 def regexPatternsFromFile(regex_input):
+    """
+    note: parse regex input arguement
+    input:
+        'regex_input': file passed from commandline arg
+    return: dictionary of labels and patterns
+    """
+
     pattenFile = open(regex_input, 'r')
 
     pattern_dict = {}
@@ -35,22 +68,35 @@ def regexPatternsFromFile(regex_input):
     return pattern_dict
 
 
-# # function to find syntactic in sentence for possible relations
 def possibleRelations(sentence):
-
-    # # subtree: a sequence of all the token's syntactic descendants.
+    """
+    note: find syntactic dependencies in sentence for possible relationsself. A
+        subtree is a sequence of all the token's syntactic descendants.
+    input:
+        'sentence': a tokenized sentence.
+    return: list of all possible dependents.
+    """
 
     subtree = []
 
     [subtree.append(str(x.ent_type_).strip() + ":" + str(x).strip())\
-        for x in sentence.subtree if str(x.ent_type_).strip() != "" and str(x).strip() != ""]
+        for x in sentence.subtree if str(x.ent_type_).strip() != "" \
+            and str(x).strip() != ""]
 
     return subtree
 
 
-# # takes an NLP object and regex pattern file as input
-# # returns a dataframe of position, type, and value
+################################## Entity Search ###############################
 def entitySearch(document, regex_input):
+    """
+    note: search through document and construct dataframe for all PII elements.
+    inputs:
+        'document': an NLP object.
+        'regex_input': regex pattern file
+
+    return: a dataframe of entities and metadata.
+    """
+
     entitySearchTime= time.time()
 
     print('Conducting entity searches...')
@@ -61,7 +107,7 @@ def entitySearch(document, regex_input):
     # # get regex patterns from file (more easily extendable)
     regex_patterns_dict = regexPatternsFromFile(regex_input)
 
-    # # iterating through sentences seems to be faster
+    #---------------------------- NER Search ----------------------------------#
     for sentence in tqdm(document.sents):
 
         # # check if sentence contains named entities
@@ -78,7 +124,11 @@ def entitySearch(document, regex_input):
                     'n_possible_dependents': len(possibleRelations(sentence))
                     }) for x in entity_list]
 
-
+    #----------------------------- Regex Search -------------------------------#
+    """
+    note: regex patterns are called on raw string values for each sent AND on
+        cleaned sentences to maximize the chances of discovering PII elements.
+    """
         for search_pattern in regex_patterns_dict:
 
             # # need to declare the string each time, not sure why
@@ -117,19 +167,25 @@ def entitySearch(document, regex_input):
 
     entitySearchTimeEnd= time.time()
     print('\nFOUND: ', len(new_rows))
-    print('Entity searches COMPLETE: ' + str(modelLoadTimeEnd - modelLoadTime) + ' seconds\n')
+    print('Entity searches COMPLETE: ' + str(modelLoadTimeEnd - modelLoadTime) \
+        + ' seconds\n')
 
     return pd.DataFrame(new_rows)
 
 
+################################## Main ########################################
 if __name__ == "__main__":
 
-    # # parse command-line args
+    #----------------------------- Argument Definition -------------------------#
     parser = argparse.ArgumentParser(description='file')
-    parser.add_argument("--input_file", help="Choose the text file to process.")
-    parser.add_argument("--regex_input", help="Choose the text file to process.")
+    parser.add_argument("--input_file", \
+        help="Choose the text file to process.")
+
+    parser.add_argument("--regex_input", \
+        help="Choose the text file to process.")
     args = parser.parse_args()
 
+    #----------------------------- NLP Model Loading --------------------------#
     totalTime = time.time()
     modelLoadTime = time.time()
 
@@ -140,8 +196,10 @@ if __name__ == "__main__":
     nlp = spacy.load(model)
     modelLoadTimeEnd = time.time()
 
-    print('Library load COMPLETE: ' + str(modelLoadTimeEnd - modelLoadTime) + ' seconds\n')
+    print('Library load COMPLETE: ' + str(modelLoadTimeEnd - modelLoadTime) \
+        + ' seconds\n')
 
+    #----------------------------- File Operations ----------------------------#
     # file operstions for naming output
     source_file = os.path.basename(args.input_file)
     base = os.path.splitext(source_file)[0]
@@ -167,7 +225,9 @@ if __name__ == "__main__":
             ' must be distributed between ' + str(splits) + ' splits...')
 
         # # add all splits to new list
-        [text_list.append(x) for x in textSplit(text, math.ceil(len(text)/splits))]
+        [text_list.append(x) for x in \
+            textSplit(text, math.ceil(len(text)/splits))]
+
         print('Document split COMPLETE.\n')
     else:
         # # if document under 10000000 chars, just store
@@ -179,7 +239,8 @@ if __name__ == "__main__":
     processing_count = 1
 
     fileReadTimeEnd = time.time()
-    print('Read COMPLETE: ' + str(fileReadTimeEnd - fileReadTime) + ' seconds\n')
+    print('Read COMPLETE: ' + str(fileReadTimeEnd - fileReadTime) \
+        + ' seconds\n')
 
     # # process all string chunks
     for string_chunk in text_list:
@@ -189,10 +250,13 @@ if __name__ == "__main__":
         spacyObjectTime = time.time()
         document = nlp(string_chunk)
         spacyObjectTimeEnd = time.time()
-        print('Convert COMPLETE: ' + str(spacyObjectTimeEnd - spacyObjectTime) + ' seconds\n')
+        print('Convert COMPLETE: ' + \
+            str(spacyObjectTimeEnd - spacyObjectTime) + ' seconds\n')
 
         # # increment the document chunk
         processing_count += 1
+
+    #----------------------------- Entity Search ------------------------------#
 
         # get dataframe with entity type, entity value, and position
         frame = entitySearch(document, args.regex_input)
